2ff0d1259bf3d85b8f811e00ef54e09eca9fd1b0
senanurkorkutan <snnrkrk1@gmail.com>
2024-05-08 11:09:41 +0200

    file structure

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 // IMPORTING
 const express = require("express");
 const app = express();
+const axios = require("axios");
+
+// .ENV FILE
+const dotenv = require("dotenv");
+dotenv.config();
 
 // PORT
-const PORT = process.env.PORT || 4444;
+const PORT = process.env.PORT || 3333;
 
 // BASIC SETTINGS
 
@@ -14,52 +19,59 @@ app.set("views", "./views");
 // om public zichtbaar te maken
 app.use(express.static("public"));
 
-// .ENV FILE
-const dotenv = require("dotenv");
-dotenv.config();
-
-// API
-//
-//
-
 // ROUTING
-app.get("/", (req, res) => {
-    console.log(req.query);
-    res.render("index", { title: "Homety" });
-    // res.send("HoOo")
+app.get("/", async (req, res) => {
+    res.render("pages/index", { title: "TEST" });
 });
 
-
-app.get("/home", (req, res) => {
-    console.log(req.query);
-    res.render("index", { title: "Homety" });
+// BLOGS
+app.get("/kilian", async (req, res) => {
+    res.render("pages/kilian", { title: "kilian" });
 });
 
-app.get("/about", (req, res) => {
-    res.send("About");
+app.get("/fenna", async (req, res) => {
+    res.render("pages/fenna", { title: "fenna" });
+});
+app.get("/hackathon", async (req, res) => {
+    res.render("pages/hackathon", { title: "hackathon" });
+});
+app.get("/jeffrey", async (req, res) => {
+    res.render("pages/jeffrey", { title: "jeffrey"});
+});
+app.get("/jeremy", async (req, res) => {
+    res.render("pages/jeremy", { title: "jeremy" });
+});
+app.get("/marieke", async (req, res) => {
+    res.render("pages/marieke", { title: "marieke" });
+});
+app.get("/nils", async (req, res) => {
+    res.render("pages/nils", { title: "nils" });
+});
+app.get("/rosa", async (req, res) => {
+    res.render("pages/rosa", { title: "rosa" });
 });
 
-// app.get("/:id", (req, res) => {
-//     res.send(req.params.id);
+// app.get("/blog/:id/", async (req, res) => {
+
 // });
 
-app.listen(PORT, () => {
-    console.log(`Example app listening on port ${PORT}`);
-});
 
-// LAATSTE ROUTE ERROR(404) PAGINA
-// app.get("/*", (req, res) => {
-//     res.status(404).render("404", { title: "404" });
-// });
 
+
+
+// ROUTE ERROR(404) PAGINA
 app.use(function (req, res) {
     res.locals.title = "Error 404";
-    res.status(404).render("404", {
+    res.status(404).render("pages/404", {
         path: "Error",
     });
 });
 
+app.get("/*", (req, res) => {
+    res.status(404).render("404", { title: "404" });
+});
+
 // PORT
-// app.listen(PORT, () => {
-//     console.log("App running on port", PORT);
-// });
+app.listen(PORT, () => {
+    console.log(`WeeklyNerd app listening on port ${PORT}`);
+});
